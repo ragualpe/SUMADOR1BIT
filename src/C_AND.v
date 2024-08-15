104d714b3fc18cbda715f4bf30ab44131dcdfcbf
ragualpe <rafael.ap@matamoros.tecnm.mx>
2024-08-15 18:53:07 -0500

    Add files via upload

---
+++ src/C_AND.v
@@ -0,0 +1,7 @@
+module C_AND (A,B,OUT);
+  input A;
+  input B;
+  output OUT;
+  assign OUT = A&B;
+  
+endmodule
---
+++ src/C_XOR.v
@@ -0,0 +1,7 @@
+module C_XOR (A,B,OUT);
+  input A;
+  input B;
+  output OUT;
+  assign OUT = A^B;
+  
+endmodule
